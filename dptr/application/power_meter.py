e3acfd01e960ee11f0886a64e6ff7053192ff052
yangfurong <yangfurong@ict.ac.cn>
2018-09-28 00:45:47 +0200

    modify application server

--- dptr/application/power_meter.py
+++ dptr/application/power_meter.py
@@ -6,6 +6,7 @@ import logging
 import threading
 import argparse
 import socket
+from urllib import request
 from threading import Thread
 
 sys.path.append(os.path.abspath(os.path.join("..", "..")))
@@ -14,13 +15,16 @@ from common.Def import *
 logging.basicConfig(level=logging.DEBUG)
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", type=int, required=True, help="translator will be running on which port")
+parser.add_argument("--web_addr", required=True, help="web address")
+parser.add_argument("--web_port", required=True, help="web port")
 args = parser.parse_args()
 
 HOST_ADDR = ""
 HOST_PORT = args.port
-#TODO: add cmd arguments
-WEB_ADDR = ""
-WEB_PORT = 80
+WEB_ADDR = args.web_addr
+WEB_PORT = args.web_port
+
+dev_id_map = {}
 
 class service(socketserver.BaseRequestHandler):
 
@@ -30,42 +34,47 @@ class service(socketserver.BaseRequestHandler):
             #8B devid 8B usage#
             dev_id, usage = struct.unpack("=Qd", data)
             #client address
-            #pack data (devid, client_ipv4, client_port, usage)
-            #TODO: write the code
-            return None
+            addr, port = self.client_address
+            return dev_id, usage, addr, port
 
-        def __tx_data_to_webserver(websock, appsock):
-            data = appsock.recv(16) #8B devid and 8B usage
-            report = __prepare_report(data)
-            websock.sendall(report)
+        def __rx_data_from_webserver(appsock, dev_id):
+            url = "{}:{}/recv?id={}".format(WEB_ADDR, WEB_PORT, hex(dev_id))
+            with request.urlopen(url) as f:
+                response = f.read().decode("utf-8")
+                if response == "start":
+                    ret = struct.pack("!B", 1)
+                else:
+                    ret = struct.pack("!B", 0)
+                appsock.sendall(ret)
 
-        def __rx_data_from_webserver(websock, appsock):
-            cmd = websock.recv(2) #2B cmd(type, value)
-            appsock.sendall(cmd)
 
-        def __rx_thr_main(websock, appsock):
+        def __rx_thr_main(appsock, dev_id):
             while True:
                 try:
-                    __rx_data_from_webserver(websock, appsock)
+                    __rx_data_from_webserver(appsock, dev_id)
                 except Exception as e:
-                    print(e)
-                    break
+                    logging.debug("recv from webserver timeout!");
+                    logging.debug(e)
 
-        #create a connection to webserver
-        websock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        websock.connect((WEB_ADDR, WEB_PORT))
+        def __tx_data_to_webserver(appsock):
+            data = appsock.recv(16) #8B devid and 8B usage
+            dev_id, usage, addr, port= __prepare_report(data)
 
-        # start a rx thread for webserver
-        threading.Thread(target=__rx_thr_main, args=(websock, self.request)).start()
+            if dev_id not in dev_id_map:
+                #luanch a thread
+                dev_id_map[dev_id] = True
+                threading.Thread(target = __rx_thr_main, args=(appsock, dev_id)).start()
+
+            url = "{}:{}/send?id={}&addr={}:{}&data={:.4f}".format(WEB_ADDR, WEB_PORT, hex(dev_id), addr, port, usage)
+            with request.urlopen(url) as f:
+                logging.debug("report date to server: {}".format(url))
+
 
         logging.info("accept a connection from a device")
 
         while True:
             try:
-                #data = self.request.recv(1024)
-                #logging.debug(data)
-                #self.request.sendall(data)
-                __tx_data_to_webserver(websock, self.request)
+                __tx_data_to_webserver(self.request)
             except Exception as e:
                 print(e)
                 break
