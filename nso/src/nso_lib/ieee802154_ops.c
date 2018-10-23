e0dc5d845ee0528d3137f406bf3d62e6211c2cce
lucadm94 <luca.demori.it@gmail.com>
2018-10-23 17:02:40 +0200

    added ieee802154 headers

--- nso/src/nso_lib/ieee802154_ops.c
+++ nso/src/nso_lib/ieee802154_ops.c
@@ -1,4 +1,4 @@
-#include "i802154_ops.h"
+#include "ieee802154_ops.h"
 #include "log.h"
 #include <arpa/inet.h>
 #include <linux/if_packet.h>
@@ -10,9 +10,9 @@
 #include <unistd.h>
 
 /* interface handle will be returned by handle argument*/
-int i802154_open(char *name, nic_handle_t **ret_handle) {
+int ieee802154_open(char *name, nic_handle_t **ret_handle) {
 
-    i802154_handle_t *handle = malloc(sizeof(i802154_handle_t));
+    ieee802154_handle_t *handle = malloc(sizeof(ieee802154_handle_t));
 
     //open rawsocket for nic "name"
     handle->sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_IEEE802154));
@@ -81,29 +81,32 @@ err_ret_noclose:
     return -1;
 }
 
-int i802154_close(nic_handle_t *handle) {
-    i802154_handle_t *i802154_handle = (i802154_handle_t*)handle;
-    close(i802154_handle->sockfd);
-    free(i802154_handle);
+int ieee802154_close(nic_handle_t *handle) {
+    ieee802154_handle_t *ieee802154_handle = (ieee802154_handle_t*)handle;
+    close(ieee802154_handle->sockfd);
+    free(ieee802154_handle);
     return 0;
 }
 
-int i802154_send(nic_handle_t *handle, packet_t *pkt, l2addr_t *dst) {
-    i802154_handle_t *i802154_handle = (i802154_handle_t*)handle;
+int ieee802154_send(nic_handle_t *handle, packet_t *pkt, l2addr_t *dst) {
+    ieee802154_handle_t *ieee802154_handle = (ieee802154_handle_t*)handle;
     struct sockaddr_ll sock_addr;
     memset(&sock_addr, 0, sizeof(sock_addr));
     sock_addr.sll_family = AF_PACKET;
-    sock_addr.sll_ifindex = i802154_handle->if_index;
-    sock_addr.sll_halen = IEEE802154_ALEN
+    sock_addr.sll_ifindex = ieee802154_handle->if_index;
+    sock_addr.sll_halen = IEEE802154_LONG_ADDRESS_LEN;
 
     if (pkt->data - pkt->buf < sizeof(struct ethhdr)) {
         LOG_DEBUG("headroom is not enough\n");
         return -1;
     }
     
-    //add ethernet header
-    struct ethhdr *eth = (struct ethhdr*)(pkt->data - sizeof(struct ethhdr));
-    memcpy(eth->h_source, i802154_handle->if_mac, IEEE802154_ALEN);
+    //add ieee802154 header
+    uint8_t *hdr = calloc(IEEE802154_MAX_HDR_LEN*sizeof(uint8_t));
+
+    ieee802154_set_frame_hdr
+
+    memcpy(eth->h_source, ieee802154_handle->if_mac, IEEE802154_ALEN);
     memcpy(eth->h_dest, dst->addr, IEEE802154_ALEN);
     eth->h_proto = htons(ETH_TYPE_NSO);
     pkt->data = (uint8_t*)eth;
@@ -111,29 +114,29 @@ int i802154_send(nic_handle_t *handle, packet_t *pkt, l2addr_t *dst) {
 
     memcpy(sock_addr.sll_addr, eth->h_dest, IEEE802154_ALEN);
     
-    if (sendto(i802154_handle->sockfd, pkt->data, pkt->byte_len, 0, (struct sockaddr*)&sock_addr, sizeof(sock_addr)) < 0) {
+    if (sendto(ieee802154_handle->sockfd, pkt->data, pkt->byte_len, 0, (struct sockaddr*)&sock_addr, sizeof(sock_addr)) < 0) {
         LOG_DEBUG("send packet failed\n");
         return -1;
     }
     return 0;
 }
 
-int i802154_broadcast(nic_handle_t *handle, packet_t *pkt) {
+int ieee802154_broadcast(nic_handle_t *handle, packet_t *pkt) {
     uint8_t bc_mac[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
     l2addr_t *addr = alloc_l2addr(IEEE802154_ALEN, bc_mac);
-    int ret = i802154_send(handle, pkt, addr);
+    int ret = ieee802154_send(handle, pkt, addr);
     free_l2addr(addr);
     return ret;
 }
 
-int i802154_receive(nic_handle_t *handle, packet_t *pkt, l2addr_t **src, l2addr_t **dst) {
-    i802154_handle_t *i802154_handle = (i802154_handle_t*)handle;
+int ieee802154_receive(nic_handle_t *handle, packet_t *pkt, l2addr_t **src, l2addr_t **dst) {
+    ieee802154_handle_t *ieee802154_handle = (ieee802154_handle_t*)handle;
     if (pkt->data - pkt->buf < sizeof(struct ethhdr)) {
         LOG_DEBUG("headroom of packet is not enough!\n");
         return -1;
     }
     pkt->data -= sizeof(struct ethhdr);
-    int numbytes = recvfrom(i802154_handle->sockfd, pkt->data, pkt->size + sizeof(struct ethhdr), 0, NULL, NULL);
+    int numbytes = recvfrom(ieee802154_handle->sockfd, pkt->data, pkt->size + sizeof(struct ethhdr), 0, NULL, NULL);
     if (numbytes <= 0) {
         LOG_DEBUG("unable to receive a valid packet\n");
         goto err_ret;
@@ -158,8 +161,8 @@ err_ret:
     return -1;
 }
 
-int i802154_get_info(nic_handle_t *handle, nic_info_t *info) {
-    i802154_handle_t *i802154_hdl = (i802154_handle_t*)handle;
-    info->mtu = i802154_hdl->if_mtu;
+int ieee802154_get_info(nic_handle_t *handle, nic_info_t *info) {
+    ieee802154_handle_t *ieee802154_hdl = (ieee802154_handle_t*)handle;
+    info->mtu = ieee802154_hdl->if_mtu;
     return 0;
 }
